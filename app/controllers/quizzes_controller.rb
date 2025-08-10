c4c1f8e27e532a6a68eb03d2df74d410fd7a4421
Toms <78614846+TomTeraud@users.noreply.github.com>
2025-08-10 13:28:23 +0000

    add: quizzes can be deleted

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -36,6 +36,11 @@ class QuizzesController < ApplicationController
   end
 
   def destroy
+    @quiz = Quiz.find(params[:id])
+
+    @quiz.destroy
+
+    redirect_to quizzes_path
   end
 
   def quiz_params
