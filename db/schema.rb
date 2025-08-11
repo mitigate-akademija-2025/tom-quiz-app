7c451b3c14694f48ac83d0924a592a7f99f1ec23
Toms <78614846+TomTeraud@users.noreply.github.com>
2025-08-11 05:51:39 +0000

    fix: migration init, quiz legacy mock data vipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_11_045514) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_11_053212) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -25,5 +25,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_11_045514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "difficulty", default: 1
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_quizzes_on_category_id"
   end
+
+  add_foreign_key "quizzes", "categories"
 end
