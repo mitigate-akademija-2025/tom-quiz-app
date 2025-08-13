76aed75d7478df7b652abb7cbfad7cae58cfa9a0
Toms <78614846+TomTeraud@users.noreply.github.com>
2025-08-13 15:35:25 +0000

    add:  take view, answer state managed in session

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,6 @@
 class QuizzesController < ApplicationController
-  before_action :set_quiz, only: [ :show, :edit, :update, :destroy ]
-  before_action :set_categories, only: [ :new, :edit ]
+  before_action :set_quiz, only: [ :show, :edit, :update, :destroy, :start, :take, :answer, :results ]
+  before_action :set_categories, only: [ :new, :edit, :create, :update ]
 
   def index
     @quizzes = Quiz.all
@@ -21,7 +21,7 @@ class QuizzesController < ApplicationController
     if @quiz.save
       redirect_to @quiz, notice: "Quiz was successfully created.", status: :see_other
     else
-      set_categories  # Reload categories for form
+      # @categories already loaded by before_action
       render :new, status: :unprocessable_content
     end
   end
@@ -34,7 +34,7 @@ class QuizzesController < ApplicationController
     if @quiz.update(quiz_params)
       redirect_to @quiz, notice: "Quiz was successfully updated.", status: :see_other
     else
-      set_categories  # Reload categories for form
+      # @categories already loaded by before_action
       render :edit, status: :unprocessable_content
     end
   end
@@ -45,59 +45,62 @@ class QuizzesController < ApplicationController
   end
 
   def start
-    @quiz = Quiz.find(params[:id])
+    # @quiz already set by before_action
     session[:quiz_attempt] = nil  # Clear previous attempt
   end
 
   def take
-    @quiz = Quiz.find(params[:id])
+    # @quiz already set by before_action
     @current_question_number = params[:question]&.to_i || 1
     @question = @quiz.questions.order(:created_at)[@current_question_number - 1]
     @total_questions = @quiz.questions.count
 
-Rails.logger.debug "=== TAKE ACTION ==="
-  Rails.logger.debug "Current question: #{@current_question_number}"
-  Rails.logger.debug "Question ID: #{@question.id}"
-  Rails.logger.debug "Session data: #{session[:quiz_attempt]}"
-  Rails.logger.debug "Looking for answer: #{session.dig(:quiz_attempt, :answers, @question.id.to_s)}"
+    Rails.logger.debug "=== TAKE ACTION ==="
+    Rails.logger.debug "Current question: #{@current_question_number}"
+    Rails.logger.debug "Question ID: #{@question&.id}"
+    Rails.logger.debug "Session data: #{session[:quiz_attempt]}"
+    Rails.logger.debug "Looking for answer: #{session.dig(:quiz_attempt, 'answers', @question&.id.to_s)}"
   end
 
   def answer
-  session[:quiz_attempt] ||= {}
-  session[:quiz_attempt]["answers"] ||= {}
-  session[:quiz_attempt]["answers"][params[:question_id]] = params[:answer_ids]
-  session[:quiz_attempt]["current_question"] = params[:current_question]
+    # Initialize session structure if needed
+    session[:quiz_attempt] ||= {}
+    session[:quiz_attempt]["answers"] ||= {}
 
+    # Store the answer
+    session[:quiz_attempt]["answers"][params[:question_id]] = params[:answer_ids]
+    session[:quiz_attempt]["current_question"] = params[:current_question]
 
     Rails.logger.debug "=== ANSWER ACTION ==="
-  Rails.logger.debug "Question ID: #{params[:question_id]}"
-  Rails.logger.debug "Answer ID: #{params[:answer_ids]}"
-  Rails.logger.debug "Session after storing: #{session[:quiz_attempt]}"  
+    Rails.logger.debug "Question ID: #{params[:question_id]}"
+    Rails.logger.debug "Answer ID: #{params[:answer_ids]}"
+    Rails.logger.debug "Session after storing: #{session[:quiz_attempt]}"
 
-  next_question = params[:current_question].to_i + 1
-  quiz = Quiz.find(params[:id])
-  
-  if next_question <= quiz.questions.count
-    redirect_to take_quiz_path(params[:id], question: next_question)
-  else
-    redirect_to results_quiz_path(params[:id])
+    next_question = params[:current_question].to_i + 1
+
+    # @quiz already set by before_action
+    if next_question <= @quiz.questions.count
+      redirect_to take_quiz_path(@quiz, question: next_question)
+    else
+      redirect_to results_quiz_path(@quiz)
+    end
   end
-end
 
-def results
-  @quiz = Quiz.find(params[:id])
-  @answers = session[:quiz_attempt]&.dig(:answers) || {}
-  @score = calculate_score(@quiz, @answers)
-  
-  # Store best score
-  session[:best_scores] ||= {}
-  current_best = session[:best_scores][@quiz.id.to_s] || 0
-  session[:best_scores][@quiz.id.to_s] = [@score[:percentage], current_best].max
-  
-  session[:quiz_attempt] = nil
-end
+  def results
+    # @quiz already set by before_action
+    @answers = session.dig(:quiz_attempt, "answers") || {}
+    @score = calculate_score(@quiz, @answers)
 
-private
+    # Store best score
+    session[:best_scores] ||= {}
+    current_best = session[:best_scores][@quiz.id.to_s] || 0
+    session[:best_scores][@quiz.id.to_s] = [ @score[:percentage], current_best ].max
+
+    # Clear the current attempt
+    session[:quiz_attempt] = nil
+  end
+
+  private
 
   def set_quiz
     @quiz = Quiz.find(params[:id])
@@ -110,19 +113,22 @@ private
   def quiz_params
     params.expect(quiz: [ :title, :description, :category_id ])
   end
-end
 
-def calculate_score(quiz, answers)
-  correct = 0
-  total = quiz.questions.count
-  
-  quiz.questions.each do |question|
-    user_answer_id = answers[question.id.to_s]
-    if user_answer_id && question.answers.find_by(id: user_answer_id)&.is_correct?
-      correct += 1
+  def calculate_score(quiz, answers)
+    correct = 0
+    total = quiz.questions.count
+
+    quiz.questions.each do |question|
+      user_answer_id = answers[question.id.to_s]
+      if user_answer_id && question.answers.find_by(id: user_answer_id)&.is_correct?
+        correct += 1
+      end
     end
-  end
-  
-  { correct: correct, total: total, percentage: (correct.to_f / total * 100).round }
-end
 
+    {
+      correct: correct,
+      total: total,
+      percentage: total > 0 ? (correct.to_f / total * 100).round : 0
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/quizzes/:id/start', to: 'quizzes#start' , as: 'start_quiz' 
-  get '/quizzes/:id/take', to: 'quizzes#take', as: 'take_quiz'
-  post '/quizzes/:id/answer', to: 'quizzes#answer', as: 'answer_quiz'
-  get '/quizzes/:id/results', to: 'quizzes#results', as: 'results_quiz'
-  
+  get "/quizzes/:id/start", to: "quizzes#start", as: "start_quiz"
+  get "/quizzes/:id/take", to: "quizzes#take", as: "take_quiz"
+  post "/quizzes/:id/answer", to: "quizzes#answer", as: "answer_quiz"
+  get "/quizzes/:id/results", to: "quizzes#results", as: "results_quiz"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
