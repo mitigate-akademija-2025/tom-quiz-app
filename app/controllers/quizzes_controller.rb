7cc7e719174413dce3409694adec2b83b8c3b169
Toms <78614846+TomTeraud@users.noreply.github.com>
2025-08-13 14:15:13 +0000

    tmp

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -46,9 +46,58 @@ class QuizzesController < ApplicationController
 
   def start
     @quiz = Quiz.find(params[:id])
+    session[:quiz_attempt] = nil  # Clear previous attempt
   end
 
-  private
+  def take
+    @quiz = Quiz.find(params[:id])
+    @current_question_number = params[:question]&.to_i || 1
+    @question = @quiz.questions.order(:created_at)[@current_question_number - 1]
+    @total_questions = @quiz.questions.count
+
+Rails.logger.debug "=== TAKE ACTION ==="
+  Rails.logger.debug "Current question: #{@current_question_number}"
+  Rails.logger.debug "Question ID: #{@question.id}"
+  Rails.logger.debug "Session data: #{session[:quiz_attempt]}"
+  Rails.logger.debug "Looking for answer: #{session.dig(:quiz_attempt, :answers, @question.id.to_s)}"
+  end
+
+  def answer
+  session[:quiz_attempt] ||= {}
+  session[:quiz_attempt]["answers"] ||= {}
+  session[:quiz_attempt]["answers"][params[:question_id]] = params[:answer_ids]
+  session[:quiz_attempt]["current_question"] = params[:current_question]
+
+
+    Rails.logger.debug "=== ANSWER ACTION ==="
+  Rails.logger.debug "Question ID: #{params[:question_id]}"
+  Rails.logger.debug "Answer ID: #{params[:answer_ids]}"
+  Rails.logger.debug "Session after storing: #{session[:quiz_attempt]}"  
+
+  next_question = params[:current_question].to_i + 1
+  quiz = Quiz.find(params[:id])
+  
+  if next_question <= quiz.questions.count
+    redirect_to take_quiz_path(params[:id], question: next_question)
+  else
+    redirect_to results_quiz_path(params[:id])
+  end
+end
+
+def results
+  @quiz = Quiz.find(params[:id])
+  @answers = session[:quiz_attempt]&.dig(:answers) || {}
+  @score = calculate_score(@quiz, @answers)
+  
+  # Store best score
+  session[:best_scores] ||= {}
+  current_best = session[:best_scores][@quiz.id.to_s] || 0
+  session[:best_scores][@quiz.id.to_s] = [@score[:percentage], current_best].max
+  
+  session[:quiz_attempt] = nil
+end
+
+private
 
   def set_quiz
     @quiz = Quiz.find(params[:id])
@@ -62,3 +111,18 @@ class QuizzesController < ApplicationController
     params.expect(quiz: [ :title, :description, :category_id ])
   end
 end
+
+def calculate_score(quiz, answers)
+  correct = 0
+  total = quiz.questions.count
+  
+  quiz.questions.each do |question|
+    user_answer_id = answers[question.id.to_s]
+    if user_answer_id && question.answers.find_by(id: user_answer_id)&.is_correct?
+      correct += 1
+    end
+  end
+  
+  { correct: correct, total: total, percentage: (correct.to_f / total * 100).round }
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   end
 
   get '/quizzes/:id/start', to: 'quizzes#start' , as: 'start_quiz' 
+  get '/quizzes/:id/take', to: 'quizzes#take', as: 'take_quiz'
+  post '/quizzes/:id/answer', to: 'quizzes#answer', as: 'answer_quiz'
+  get '/quizzes/:id/results', to: 'quizzes#results', as: 'results_quiz'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
