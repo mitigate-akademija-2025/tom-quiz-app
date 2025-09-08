5667ab49ad23b77306488c839698638f7af811a2
Tom <tomsterauds@gmail.com>
2025-09-08 17:56:12 +0300

    add: signup/in option with google OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,13 @@ rescue => e
   github_creds = nil
 end
 
+begin
+  google_creds = Rails.application.credentials.google_oauth2
+rescue => e
+  warn "WARN: Could not load Google credentials. Error: #{e.message}"
+  google_creds = nil
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
 
@@ -12,5 +19,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :github, github_creds[:client_id], github_creds[:client_secret]
   else
     warn "WARN: GitHub OmniAuth provider is not configured."
+  end
+
+  if google_creds&.key?(:client_id) && google_creds&.key?(:client_secret)
+    provider :google_oauth2, google_creds[:client_id], google_creds[:client_secret]
+  else
+    warn "WARN: Google OAuth2 provider is not configured."
   end
 end
